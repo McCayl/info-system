543234c0477745a4d46b015efbc1c18622c357c7
putilove <gleb89047408502@gmail.com>
2022-01-09 20:34:30 +0400

    add main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import controller.Controller;
+
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
+
 import model.Model;
 import model.Music;
 import view.View;
 
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Controller ctrl = new Controller(new Model(), new View());
+        ctrl.workWithFirstLvlMenu();
     }
 }
