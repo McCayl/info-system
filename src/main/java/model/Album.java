03e66bc5346cb63bc7a4c455f072fed714dbcba8
putilove <gleb89047408502@gmail.com>
2022-01-09 20:33:42 +0400

    add functions

--- src/main/java/model/Album.java
+++ src/main/java/model/Album.java
@@ -6,9 +6,24 @@ import java.util.LinkedList;
 public class Album implements Serializable {
     private LinkedList <Music> trackList;
     private String title;
-    
+
+    public Album(){
+        trackList = new LinkedList<>();
+    }
     public LinkedList <Music> getTrackList() { return trackList; }
     public void setTrackList(LinkedList <Music> trackList) { this.trackList = trackList; }
+    public void addTrack(Music track) {
+        if (trackList.contains(track)) {
+            System.out.println("This track is already exist");
+            return;
+        }
+        if (!(track.getAlbumTitle().equals("not"))) {
+            trackList.add(track);
+        }
+        else {
+            System.out.println("This track is already on the album");
+        }
+    }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
      
