9ac2170a800395c316d7a9940b217d4a6c41a384
putilove <gleb89047408502@gmail.com>
2022-01-09 20:34:06 +0400

    add print menu

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import model.Album;
 import model.Music;
@@ -7,7 +8,7 @@ import model.Music;
 public class View {
 
     /**
-     * Need menu, print methods or something :^)
+     * Need menu, print methods or something :^) -Ok, it will be done
      */
 
     public void printWrongMessage() {
@@ -18,30 +19,34 @@ public class View {
         System.out.println("This track is already exist");
     }
 
-    public void printTrack(Music track) {
-        System.out.printf("%s %s %s %s %d",
-                track.getTitle(),
-                track.getAuthor(),
-                track.getGenre(),
-                track.getAlbumTitle(),
-                track.getLength());
+    public void actionFailed() {
+        System.out.println("Action failed due to user error");
     }
 
-    public void printNotAlbumTrack(Music track) {
-        System.out.printf("%s %s %s %d",
-                track.getTitle(),
-                track.getAuthor(),
-                track.getGenre(),
-                track.getLength());
+    public void actionSuccess() {
+        System.out.println("Action successfully completed");
+    }
+
+    public void wrongKeyTyped() {
+        System.out.println("You typed to incorrect key, please try again ;)");
+    }
+
+    public void noTracksAvailable() {
+        System.out.println("There are no tracks that are not in the album");
+    }
+
+    public void printTrack(Music track) {
+        System.out.println(track);
     }
 
     public void printTrackList(LinkedList <Music> trackList) {
+        if(trackList == null) {
+            System.out.println("No tracks\n");
+            return;
+        }
         for (int i = 0; i != trackList.size(); i++) {
-            if (!(trackList.get(i).getAlbumTitle().equals("not"))) {
-                printTrack(trackList.get(i));
-            } else {
-                printNotAlbumTrack(trackList.get(i));
-            }
+            System.out.print((i+1) + ". ");
+            printTrack(trackList.get(i));
         }
     }
     
@@ -51,10 +56,122 @@ public class View {
     }
     
     public void printListOfAlbums(LinkedList <Album> albums) {
-        System.out.println("Album title list");
+        if(albums == null) {
+            System.out.println("No albums\n");
+            return;
+        }
+        System.out.println("Album title list: ");
         for (int i = 0; i != albums.size(); i++) {
-            System.out.printf("%s ", albums.get(i).getTitle());
+            System.out.printf(i+1 + ". %s\n", albums.get(i).getTitle());
+        }
+        System.out.println("\n");
+    }
+
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("cls");
+        } catch (IOException | InterruptedException ex) {
+            ex.printStackTrace();
         }
     }
-    
+
+    //add set del serialize
+    public void printFirstLvlMenu(int codeOfResult) throws IOException {
+        clearScreen();
+        if(codeOfResult == -1){
+            wrongKeyTyped();
+        }
+        else if(codeOfResult == -2){
+            actionFailed();
+        }
+        else if(codeOfResult == 1){
+            actionSuccess();
+        }
+        System.out.println("\tMusic Library by 32\n\n");
+        System.out.println("  Choose action:");
+        System.out.println("1. Show Tracks");
+        System.out.println("2. Show Albums");
+        System.out.println("3. Save");
+        System.out.println("4. Load");
+        System.out.println("0. EXIT");
+    }
+
+    public void printSecondLvlTrackMenu(LinkedList <Music> trackList, int codeOfResult) throws IOException {
+        clearScreen();
+        if(codeOfResult == -1){
+            wrongKeyTyped();
+        }
+        else if(codeOfResult == -2){
+            actionFailed();
+        }
+        else if(codeOfResult == 1){
+            actionSuccess();
+        }
+        System.out.println("\tMusic Library by 32\n");
+        printTrackList(trackList);
+        System.out.println("  Choose action:");
+        System.out.println("\n");
+        System.out.println("1. Add Track");
+        System.out.println("2. Edit Track");
+        System.out.println("3. Delete Track");
+        System.out.println("0. Back");
+    }
+
+    public void printSecondLvlAlbumMenu(LinkedList <Album> albums, int codeOfResult) throws IOException {
+        clearScreen();
+        if(codeOfResult == -1){
+            wrongKeyTyped();
+        }
+        System.out.println("\tMusic Library by 32\n");
+        printListOfAlbums(albums);
+        System.out.println("  Choose action:");
+        System.out.println("\n");
+        System.out.println("1. Add Album");
+        System.out.println("2. Edit Album");
+        System.out.println("3. Delete Album");
+        System.out.println("0. Back");
+    }
+
+    public void printAlbumAddMenu(int codeOfResult){
+        clearScreen();
+        if(codeOfResult == -1){
+            wrongKeyTyped();
+        }
+        else if(codeOfResult == -3){
+            noTracksAvailable();
+        }
+        else if(codeOfResult ==1){
+            actionSuccess();
+        }
+        System.out.println("\tMusic Library by 32\n");
+        System.out.println("  Choose action:");
+        System.out.println("\n");
+        System.out.println("1. Add a new track");
+        System.out.println("2. Add an existing track");
+    }
+
+    public void printTrackEditMenu(Music track, int codeOfResult) throws IOException {
+        clearScreen();
+        if(codeOfResult == -1){
+            wrongKeyTyped();
+        }
+        else if(codeOfResult == -2){
+            actionFailed();
+        }
+        else if(codeOfResult == 1){
+            actionSuccess();
+        }
+        System.out.println("Selected track: ");
+        printTrack(track);
+        System.out.println("\nChoose action: ");
+        System.out.println("1. Edit name");
+        System.out.println("2. Edit author");
+        System.out.println("3. Edit genre");
+        System.out.println("4. Edit length");
+        System.out.println("5. Edit name of album");
+        System.out.println("0. Back");
+    }
 }
