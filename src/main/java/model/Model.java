1c426ba70d2e7b22d41abb45dd1cc2ee84546d43
putilove <gleb89047408502@gmail.com>
2022-01-09 20:34:42 +0400

    add ctor

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -6,7 +6,13 @@ import java.util.LinkedList;
 public class Model implements Serializable {
     private LinkedList <Music> trackList;
     private LinkedList <Album> albums;
-    
+
+    public Model(){
+        this.trackList = new LinkedList<>();
+        this.albums = new LinkedList<>();
+
+    }
+
     public LinkedList <Music> getTrackList() { return trackList; }
     public void setTrackList(LinkedList <Music> trackList) { this.trackList = trackList; }
     public LinkedList <Album> getAlbums() { return albums; }
