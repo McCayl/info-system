580ef6ebc52b16ba25e0b442261ab21a090754e8
putilove <gleb89047408502@gmail.com>
2022-01-21 19:53:04 +0400

    delete unnecessary fields and functions

--- src/main/java/model/Album.java
+++ src/main/java/model/Album.java
@@ -6,20 +6,13 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Album implements Serializable {
-    private ArrayList <Track> trackList;
     private String title;
     private int year;
 
-    public Album(){
-        trackList = new ArrayList<>();
-    }
     public Album(String title, int year){
         this.title = title;
         this.year = year;
-        trackList = new ArrayList<>();
     }
-    public ArrayList <Track> getTrackList() { return trackList; }
-    public void setTrackList(ArrayList <Track> trackList) { this.trackList = trackList; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public int getYear() { return year; }
@@ -31,13 +24,12 @@ public class Album implements Serializable {
         if (o == null || getClass() != o.getClass()) return false;
         Album album = (Album) o;
         return year == album.year &&
-                Objects.equal(trackList, album.trackList) &&
                 Objects.equal(title, album.title);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(trackList, title, year);
+        return Objects.hashCode(title, year);
     }
 
 }
