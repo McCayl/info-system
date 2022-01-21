bb9e2f541c960ae7e9050f076fdddc391b5e497c
putilove <gleb89047408502@gmail.com>
2022-01-21 16:25:14 +0400

    rename field (misspell)

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 public class Model implements Serializable {
     private ArrayList <Track> trackList;
     private ArrayList <Album> albums;
-    private Multimap <String, String> assosiationsMap;
+    private Multimap <String, String> associationMap;
 
     public Model() {
         trackList = new ArrayList<>();
         albums = new ArrayList<>();
-        assosiationsMap = ArrayListMultimap.create();
+        associationMap = ArrayListMultimap.create();
     }
 
     public ArrayList<Track> getTrackList() { return trackList; }
     public void setTrackList(ArrayList <Track> trackList) { this.trackList = trackList; }
     public ArrayList <Album> getAlbums() { return albums; }
     public void setAlbums(ArrayList <Album> albums) { this.albums = albums; }
-    public Multimap<String, String> getAssosiationsMap() { return assosiationsMap; }
-    public void setAssosiationsMap(Multimap <String, String> map) { assosiationsMap = map; }
+    public Multimap<String, String> getAssociationMap() { return associationMap; }
+    public void setAssociationMap(Multimap <String, String> map) { associationMap = map; }
 
 }
